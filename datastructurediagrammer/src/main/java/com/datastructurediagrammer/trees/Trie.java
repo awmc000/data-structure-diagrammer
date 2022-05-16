5c0f8c32ac0a122aec15b2b4c453eff5f85d33bf
awmc000 <mccolmalex@gmail.com>
2022-05-16 09:58:31 -0700

    Steps toward Trie implementation

--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/Trie.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/Trie.java
@@ -1,12 +1,22 @@
 package com.datastructurediagrammer.trees;
 
 public class Trie {
-    public TrieNode head;
+    public Character[] head;
+
+    public Trie() { 
+        head = new Character[26];
+    }
 
     public void add (String newString) { 
         char[] newChars = newString.toCharArray();
-        for (int i = 0; i < newChars.length; ++i) { 
-            
+        Character[] currentArray = head;
+        for (char character : newChars) { 
+            Character currentCharacter = character;
         }
+        
     }
+
+    public static int charCode(Character character) { 
+        return character - 66;
+    } 
 }
--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/TrieNode.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/TrieNode.java
@@ -2,7 +2,7 @@ package com.datastructurediagrammer.trees;
 
 public class TrieNode {
     public Character character;
-    public Character[] next; // An array of length 27 - A to Z uppercase and empty space.
+    public Character[] next; // An array of length 26 - A to Z uppercase
     public boolean isTerminal;
 
     public TrieNode(Character character, boolean isTerminal) { 
@@ -18,4 +18,8 @@ public class TrieNode {
             next[code] = character;
         }
     }
+
+    public static int charCode(Character character) { 
+        return character - 66;
+    } 
 }
