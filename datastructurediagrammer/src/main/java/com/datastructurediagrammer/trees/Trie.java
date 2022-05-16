404df1da46cce1cb1048fede41848a45db3fa779
awmc000 <mccolmalex@gmail.com>
2022-05-16 11:35:30 -0700

    further work on Trie
    Adding works, basic structure works. The contains() method does not yet work

--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/Trie.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/Trie.java
@@ -1,22 +1,86 @@
 package com.datastructurediagrammer.trees;
 
 public class Trie {
-    public Character[] head;
+    public TrieNode[] head;
 
     public Trie() { 
-        head = new Character[26];
+        head = new TrieNode[26];
     }
 
     public void add (String newString) { 
-        char[] newChars = newString.toCharArray();
-        Character[] currentArray = head;
-        for (char character : newChars) { 
-            Character currentCharacter = character;
+        // Fix up the string
+        newString = newString.toUpperCase();
+        // TODO: Get rid of numbers and non-alphabet characters.
+
+        // Split the string into a char array
+        char[] charArray = newString.toCharArray();
+
+        // Set up the integer array to hold the codes of 0 to 25
+        int[] newStringCodes = new int[charArray.length];
+
+        // Fill that array with the codes, using a static method that subtracts 65
+        for (int i = 0; i < charArray.length; ++i) { 
+            newStringCodes[i] = charCode(charArray[i]);
         }
-        
+
+        boolean oneLetterWord = false;
+        if (newString.length() == 1) { 
+            oneLetterWord = true;
+        }
+
+        head[newStringCodes[0]] = new TrieNode(oneLetterWord);
+        TrieNode currNode = head[newStringCodes[0]];
+
+        for (int i = 1; i < newStringCodes.length; ++i) { 
+            
+            int nextCode = newStringCodes[i];
+            boolean nextIsTerminal = false;
+
+            // If the string code we are currently processing is the last one in the string, be sure to flag it as terminal.
+            if (i == newStringCodes.length - 1) { 
+                nextIsTerminal = true;
+            }
+
+            // Add the current *character*.
+            currNode.add(new TrieNode(nextIsTerminal), nextCode, nextIsTerminal);
+
+            // Continue the traversal onto the next character in the sequence (Next character in string being added).
+            currNode = currNode.links[nextCode];
+        }
+    }
+
+    public boolean contains(String searchKey) { 
+        char[] charArray = searchKey.toCharArray();
+        int[] codeArray = new int[charArray.length];
+
+        for (int i = 0; i < codeArray.length; ++i) { 
+            codeArray[i] = charCode(charArray[i]); // Eg.: "HEAL" -> charArray[2] == 'A', charChode(charArray[2]) == 0
+        }
+
+        TrieNode currNode = head[codeArray[0]];
+
+        for (int i = 1; i < codeArray.length; ++i) {
+            if (currNode != null) {
+                if (currNode.links[codeArray[i]] != null) { 
+                    currNode = currNode.links[codeArray[i]];
+                }
+            }
+        }
+
+        return false;
     }
 
     public static int charCode(Character character) { 
-        return character - 66;
-    } 
+        return character - 65;
+    }
+    public static void main(String[] args) { 
+        Trie testTrie = new Trie();
+        testTrie.add("HELLO");
+        System.out.println(testTrie.contains("HELLO") + " " + testTrie.contains("A"));
+        testTrie.add("A");
+
+        System.out.println(testTrie.contains("HELLO") + " " + testTrie.contains("A"));
+        
+    }
+
 }
--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/TrieNode.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/TrieNode.java
@@ -1,25 +1,30 @@
 package com.datastructurediagrammer.trees;
 
 public class TrieNode {
-    public Character character;
-    public Character[] next; // An array of length 26 - A to Z uppercase
+    public TrieNode[] links; // An array of length 26 - A to Z uppercase
     public boolean isTerminal;
 
-    public TrieNode(Character character, boolean isTerminal) { 
-        this.character = character;
+    public TrieNode(boolean isTerminal) { 
         this.isTerminal = isTerminal;
-        this.next = new Character[27];
+        this.links = new TrieNode[27];
     }
 
-    public void add(Character character) { 
-        int code = character - 66;
+    public boolean add(TrieNode newNode, int index, boolean terminal) { 
+        // If the index is inappropriate, return false
+        if ((index < 0) || (index > 25)) { 
+            return false;
+        }
 
-        if (next[code] != null) { 
-            next[code] = character;
+        // If the desired spot in the links array is empty, put it there.
+        if (links[index] != null) { 
+            // The desired link was already there. Cool
+            links[index].isTerminal = terminal;
+            return true;
+        } else { 
+            // The next link is successfully inserted.
+            links[index] = newNode;
+            links[index].isTerminal = terminal; // If adding a terminal node, flag it as such.
+            return true;
         }
     }
-
-    public static int charCode(Character character) { 
-        return character - 66;
-    } 
 }
