8ecc263acca5634947b94fd221101d40a88b23d1
awmc000 <mccolmalex@gmail.com>
2022-05-18 21:13:47 -0700

    added diagrammer test boilerplate

---
+++ datastructurediagrammer/src/test/java/com/datastructurediagrammer/sorting/BubbleSortArrayDiagrammerTest.java
@@ -0,0 +1,12 @@
+package com.datastructurediagrammer.sorting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BubbleSortArrayDiagrammerTest {
+    @Test
+    public void basicFunctionTest() { 
+
+    }
+}
