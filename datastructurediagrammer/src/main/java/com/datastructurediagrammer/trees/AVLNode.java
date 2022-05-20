1b0eb1070d5545c25e6ebc6e07ee06dfcb67f5da
awmc000 <mccolmalex@gmail.com>
2022-05-20 11:58:59 -0700

    began implementing AVLNode

--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/AVLNode.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/AVLNode.java
@@ -1,5 +1,45 @@
 package com.datastructurediagrammer.trees;
 
-public class AVLNode {
-    
+public class AVLNode <T extends Comparable<T>> extends BSTNode<T> {
+    private int height;
+
+    public AVLNode(T initData) { 
+        super(initData);
+        height = 0;
+    }
+
+    private int[] getChildHeights() { 
+        int[] results = new int[2];
+        // Get height of left subtree, or -1 if null
+        int leftHeight = -1;
+        if (this.left != null) { 
+            AVLNode<T> leftNode = (AVLNode<T>) left;
+            leftHeight = leftNode.height;
+        }
+        results[0] = leftHeight;
+
+        // Get height of right subtree, or -1 if null.
+        int rightHeight = -1;
+        if (this.right != null) { 
+            AVLNode<T> rightNode = (AVLNode<T>) right;
+            rightHeight = rightNode.height;
+        }
+        results[1] = rightHeight;
+
+        return results;
+    }
+
+    public int getBalance() { 
+        int[] heights = getChildHeights();
+
+        // Return balance factor
+        return heights[0] - heights[1];
+    }
+
+    public void updateHeight() { 
+        int[] heights = getChildHeights();
+        height = (Math.max(heights[0], heights[1]));
+    }
+
+    //TODO: implement setChild, replaceChild, rotateLeft, rotateRight, rebalance methods
 }
--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/BSTNode.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/BSTNode.java
@@ -1,13 +1,16 @@
 package com.datastructurediagrammer.trees;
 
 public class BSTNode<T extends Comparable<T>> {
-    private T data;
+    protected T data;
     public BSTNode<T> parent;
     public BSTNode<T> left;
     public BSTNode<T> right;
 
     public BSTNode(T initData) { 
         data = initData;
+        parent = null;
+        left = null;
+        right = null;
     }
 
     public T getData() { 
