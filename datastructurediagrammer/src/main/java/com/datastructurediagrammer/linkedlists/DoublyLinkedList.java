0007084056927c425c2098f8aa88706a45acc5b9
awmc000 <mccolmalex@gmail.com>
2022-06-01 19:34:59 -0700

    Refactoring, commenting, cleanup of SLL & DLL

--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/linkedlists/DoublyLinkedList.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/linkedlists/DoublyLinkedList.java
@@ -1,14 +1,39 @@
 package com.datastructurediagrammer.linkedlists;
 
+/** A doubly-linked list. Uses DLLNode.
+ * @author Alex McColm
+ */
 public class DoublyLinkedList<T extends Comparable<T>> {
+    // Members
     public DLLNode<T> head;
     public DLLNode<T> tail;
 
+    // Default constructor
+    public DoublyLinkedList() { 
+        head = null;
+        tail = null;
+    }
+
+    // Constructor with data for a root node
+    public DoublyLinkedList(T initData) { 
+        DLLNode<T> initNode = new DLLNode<T>(initData);
+        head = initNode;
+        tail = initNode;
+    }
+
+    /**
+     * Constructs a node from given data, and puts it at the top of the list by a call to this.prepend().
+     * @param newData
+     */
     public void prependData(T newData) {
         DLLNode<T> newNode = new DLLNode<T>(newData);
         this.prepend(newNode);
     }
 
+    /**
+     * Puts given node at the top of the list.
+     * @param newNode
+     */
     public void prepend(DLLNode<T> newNode) { 
         newNode.next = head;
         head.previous = newNode;
@@ -18,12 +43,22 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Helper method.
+     * Constructs a node from given data, and puts it at 
+     * the end of the list by a call to this.append().
+     * @param newData
+     */
     public void appendData(T newData) { 
         DLLNode<T> newNode = new DLLNode<T>(newData);
 
         append(newNode);
     }
 
+    /**
+     * Puts the given node at the end of the list.
+     * @param newNode
+     */
     public void append(DLLNode<T> newNode) { 
         // Case 1: Empty list - point head and tail to new node
         if (head == null) { 
@@ -39,18 +74,44 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         }
     }
 
-    public int getLength() { 
-        DLLNode<T> currentNode = head;
-        int count = 0;
-        while (currentNode != null) { 
-            count++;
-            currentNode = (DLLNode<T>) currentNode.next;
-        }
-        return count;
+    /**
+     * Helper method for inserting a node after an existing node.
+     * @param existingNode Existing node for new node to be inserted after.
+     * @param newData Data for new node to be inserted after existing node.
+     */
+    public void insertAfterData(DLLNode<T> existingNode, T newData) { 
+        DLLNode<T> newNode = new DLLNode<T>(newData);
+        this.insertAfter(existingNode, newNode);
     }
 
-    public boolean isEmpty() { 
-        return !(head != null);
+    /**
+     * Inserts a node after an existing node.
+     * @param existingNode
+     * @param newNode
+     */
+    public void insertAfter(DLLNode<T> existingNode, DLLNode<T> newNode) { 
+        if (existingNode != null) {
+            newNode.next = existingNode.next;
+            existingNode.next = newNode;
+        } else {
+            this.append(newNode);
+        }
+    }
+
+    /**
+     * If a match exists, returns the first node with a data key
+     * matching the search key.
+     * @param searchKey
+     * @return SLLNode whose key matches search key.
+     */
+    public DLLNode<T> search (T searchKey) { 
+        DLLNode<T> currentNode = head;
+        while (currentNode != null) { 
+            if (currentNode.getData().compareTo(searchKey) == 0) { 
+                return currentNode;
+            }
+        }
+        return currentNode;
     }
 
     public DLLNode<T> remove(DLLNode<T> existingNode) { 
@@ -74,6 +135,25 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         }
 
         return existingNode;
+    }
+
+
+    /** Getter.
+     * Return length by counting nodes with an accumulator.
+     * @return int length of linked list
+     */
+    public int getLength() { 
+        DLLNode<T> currentNode = head;
+        int count = 0;
+        while (currentNode != null) { 
+            count++;
+            currentNode = (DLLNode<T>) currentNode.next;
+        }
+        return count;
+    }
+
+    public boolean isEmpty() { 
+        return !(head != null);
     }
 
     @Override
--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/linkedlists/SinglyLinkedList.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/linkedlists/SinglyLinkedList.java
@@ -1,11 +1,14 @@
 package com.datastructurediagrammer.linkedlists;
 
+/** A singly-linked list. Uses SLLNode.
+ * @author Alex McColm
+ */
 public class SinglyLinkedList<T extends Comparable<T>> {
-    // Data members
+    // Members
     public SLLNode<T> head;
     public SLLNode<T> tail;
 
-    // default constructor
+    // Default constructor
     public SinglyLinkedList() { 
         head = null; 
         tail = null;
@@ -18,11 +21,21 @@ public class SinglyLinkedList<T extends Comparable<T>> {
         tail = initNode;
     }
 
+    /**
+     * Helper method.
+     * Constructs a node from given data, and puts it at 
+     * the top of the list by a call to this.prepend().
+     * @param newData
+     */
     public void prependData(T newData) {
         SLLNode<T> newNode = new SLLNode<T>(newData);
         this.prepend(newNode);
     }
 
+    /**
+     * Puts given node at the top of the list.
+     * @param newNode
+     */
     public void prepend(SLLNode<T> newNode) { 
         // Case 1: Empty list - point head and tail to new node
         if (head == null) {
@@ -38,11 +51,21 @@ public class SinglyLinkedList<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Helper method.
+     * Constructs a node from given data, and puts it at 
+     * the end of the list by a call to this.append().
+     * @param newData
+     */
     public void appendData(T newData) { 
         SLLNode<T> newNode = new SLLNode<T>(newData);
         this.append(newNode);
     }
 
+    /**
+     * Puts the given node at the end of the list.
+     * @param newNode
+     */
     public void append(SLLNode<T> newNode) { 
         // Case 1: Empty list - point head and tail to new node
         if (head == null) { 
@@ -57,17 +80,36 @@ public class SinglyLinkedList<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Helper method for inserting a node after an existing node.
+     * @param existingNode Existing node for new node to be inserted after.
+     * @param newData Data for new node to be inserted after existing node.
+     */
     public void insertAfterData(SLLNode<T> existingNode, T newData) { 
         SLLNode<T> newNode = new SLLNode<T>(newData);
-        
+        this.insertAfter(existingNode, newNode);
+    }
+
+    /**
+     * Inserts a node after an existing node.
+     * @param existingNode
+     * @param newNode
+     */
+    public void insertAfter(SLLNode<T> existingNode, SLLNode<T> newNode) { 
         if (existingNode != null) {
             newNode.next = existingNode.next;
             existingNode.next = newNode;
         } else {
-            this.appendData(newData);
+            this.append(newNode);
         }
     }
 
+    /**
+     * If a match exists, returns the first node with a data key
+     * matching the search key.
+     * @param searchKey
+     * @return SLLNode whose key matches search key.
+     */
     public SLLNode<T> search (T searchKey) { 
         SLLNode<T> currentNode = head;
         while (currentNode != null) { 
@@ -78,24 +120,21 @@ public class SinglyLinkedList<T extends Comparable<T>> {
         return currentNode;
     }
 
-    public boolean removeAfter(SLLNode<T> existingNode) { 
-        // If given null, remove head
-        if (existingNode == null && head != null) {
-            SLLNode<T> succeedingNode = head.next;
-            head = succeedingNode;
-            if (succeedingNode != null) { 
-                tail = null;
-            }
-        } else if (existingNode.next != null) {
-            SLLNode<T> succeedingNode = existingNode.next.next;
-            existingNode.next = succeedingNode;
-            if (succeedingNode == null) { 
-                tail = null;
-            }
-        }
-        return true;
+    // TODO: Rewrite the removeAfter method.
+    /**
+     * TODO - NOT IMPLEMENTED.
+     * @param existingNode
+     * @return
+     */
+    public boolean removeAfter (SLLNode<T> existingNode) { 
+        return false;
     }
 
+
+    /** Getter.
+     * Return length by counting nodes with an accumulator.
+     * @return int length of linked list
+     */
     public int getLength() { 
         SLLNode<T> currentNode = head;
         int count = 0;
