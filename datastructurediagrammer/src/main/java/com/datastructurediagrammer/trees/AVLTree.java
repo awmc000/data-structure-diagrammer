48095a7e1a6b53ac9e9682b01da772c3d33eaf3b
awmc000 <mccolmalex@gmail.com>
2022-06-04 23:29:16 -0700

    working on implementing avl tree

--- datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/AVLTree.java
+++ datastructurediagrammer/src/main/java/com/datastructurediagrammer/trees/AVLTree.java
@@ -10,8 +10,30 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     }
     
     //TODO: implement rotateLeft
+    public void rotateLeft(AVLNode<T> node) { 
+
+    }
     
     //TODO: implement rotateRight 
-    
+    public AVLNode<T> rotateRight(AVLNode<T> a) { 
+        AVLNode<T> p = a.parent;
+        AVLNode<T> b = (AVLNode<T>) a.left;
+        a.left = b.right;
+        if (b.right != null) { 
+            b.right.parent = a;
+        }
+        b.right = a;
+        a.parent = b;
+        b.parent = p;
+        if (p != null) { 
+            if (p.left == a) { 
+                p.left = b;
+            } else {
+                p.right = b;
+            }
+        }
+        return b;
+    }
+
     //TODO: implement rebalance
 }
